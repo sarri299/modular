309dc862e233963e8535fcc0a26d767f8b5cbaef
sarri299 <sarri.upv@gmail.com>
2020-03-20 12:09:51 +0100

    Second commit

--- src/Teklatua.java
+++ src/Teklatua.java
@@ -1,4 +1,16 @@
-
+import java.util.Scanner;
 public class Teklatua {
-
+	private static Teklatua nireTeklatua=null;
+	
+	private Teklatua() {
+	}
+	
+	public Teklatua getNireTeklatua() {
+		if (nireTeklatua==null) {
+			nireTeklatua= new Teklatua();
+			
+		}
+		return nireTeklatua;
+	}
+	
 }
